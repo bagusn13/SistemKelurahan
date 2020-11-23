33f8918cc555bff5f7f01faca6258c970d85fbe5
bagusn13 <bagusn1367@gmail.com>
2020-11-23 21:57:29 +0700

    backend dashboard

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,8 +8,12 @@ class Admin extends CI_Controller
 	public function index()
 	{
 		$data = array(
-			'title' => 'Admin',
-			'isi'   => 'v_admin',
+			'title' 		=> 'Admin',
+			'isi'   		=> 'v_admin',
+			'tugas'			=> $this->db->count_all('tbl_surat_tugas'),
+			'masuk'			=> $this->db->count_all('tbl_surat_masuk'),
+			'keluar'		=> $this->db->count_all('tbl_surat_keluar'),
+			'keputusan'	=> $this->db->count_all('tbl_surat_keputusan'),
 		);
 		$this->load->view('layout/v_wrapper_backend', $data, FALSE);
 	}
